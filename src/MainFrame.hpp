a85f735cba793f4aacbae88bf979daa1e1f24874
Ali "Snophix" Balouz <ali.balouz98@gmail.com>
2025-06-30 22:38:48 +0300

    added some constants for the widgets

--- src/MainFrame.hpp
+++ src/MainFrame.hpp
@@ -1,6 +1,31 @@
 #pragma once
 #include <wx/wx.h>
 #include <wx/spinctrl.h>
+#include <string>
+
+
+// all the text
+constexpr std::string TITLE_BASE = "test";
+constexpr std::string TITLE_LOWER = "Lower!!";
+constexpr std::string TITLE_HIGHER = "Higher!!";
+constexpr std::string TITLE_ADD_BEGIN = "!!";
+constexpr std::string TITLE_ADD_LOSE = " :(";
+constexpr std::string TITLE_ADD_WIN = " :)";
+
+constexpr std::string MESSAGE_BEGIN = "Let's play!";
+constexpr std::string MESSAGE_LOWER = "It's lower";
+constexpr std::string MESSAGE_HIGHER = "It's higher";
+constexpr std::string MESSAGE_LOSE = "You lost!";
+constexpr std::string MESSAGE_WIN = "You won!";
+
+constexpr std::string SUBMESSAGE_TRIES = " tries left";
+constexpr std::string SUBMESSAGE_TRY = " try left";
+constexpr std::string SUBMESSAGE_REVEAL = "It was ";
+
+constexpr std::string BUTTON_GUESS = "Guess";
+constexpr std::string BUTTON_REPLAY = "Replay";
+
+
 
 class MainFrame : public wxFrame{
 private:
