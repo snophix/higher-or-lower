2df7b8517ea7235343e2ab89e881dc7a4860f629
Ali "Snophix" Balouz <ali.balouz98@gmail.com>
2025-07-01 13:44:30 +0300

    added the game logic for finishing and starting/resetting the game

--- src/MainFrame.cpp
+++ src/MainFrame.cpp
@@ -1,5 +1,11 @@
 #include "MainFrame.hpp"
 #include <wx/wx.h>
+#include <random>
+#include <format>
+
+
+int random_int(int min, int max);
+
 
 
 MainFrame::MainFrame() : wxFrame(NULL, wxID_ANY, "Higher Or Lower", wxDefaultPosition, wxSize(300, 250), wxDEFAULT_FRAME_STYLE & ~(wxRESIZE_BORDER | wxMAXIMIZE)) {
@@ -8,6 +14,8 @@ MainFrame::MainFrame() : wxFrame(NULL, wxID_ANY, "Higher Or Lower", wxDefaultPos
 
     setup_widgets();
     setup_sizers();
+
+    reset_game();
 }
 
 
@@ -23,7 +31,7 @@ void MainFrame::setup_widgets() {
 
     // the number picker
     numberPicherSpinCtrl = new wxSpinCtrl(mainPanel);
-    numberPicherSpinCtrl->SetRange(0, 100);
+    numberPicherSpinCtrl->SetRange(RANDOM_MIN, RANDOM_MAX);
 
     // the main button
     mainButton = new wxButton(mainPanel, wxID_ANY, "Guess");
@@ -40,8 +48,55 @@ void MainFrame::setup_sizers() {
     mainBoxSizer->Add(mainButton, wxSizerFlags().Center());
     //
     wxGridSizer* mainGridSizer = new wxGridSizer(1);
-    mainGridSizer->Add(mainBoxSizer, wxSizerFlags().Expand().Border(wxALL, 20));
+    mainGridSizer->Add(mainBoxSizer, wxSizerFlags().Expand().Border(wxALL, 20).CenterVertical());
 
     mainPanel->SetSizer(mainGridSizer);
     mainGridSizer->SetSizeHints(this);
+}
+
+
+void MainFrame::setup_binds(){
+
+}
+
+
+void MainFrame::reset_game(){
+    // initializing
+    isGaming = true;
+    randomNumberToGuess = random_int(RANDOM_MIN, RANDOM_MAX);
+    triesLeft = NUMBER_OF_TRIES;
+    // setting the different strings
+    SetTitle(TITLE_BASE + TITLE_ADD_BEGIN);
+    messageText->SetLabel(MESSAGE_BEGIN);
+    submessageText->SetLabel(std::format(SUBMESSAGE_TRIES, triesLeft));
+    numberPicherSpinCtrl->SetValue(0);
+    numberPicherSpinCtrl->Show();
+    mainButton->SetLabel(BUTTON_GUESS);
+}
+
+
+void MainFrame::finish_game(bool wonGame){
+    isGaming = false;
+    submessageText->SetLabel(std::format(SUBMESSAGE_REVEAL, randomNumberToGuess));
+    mainButton->SetLabel(BUTTON_REPLAY);
+    numberPicherSpinCtrl->Hide();
+
+    if (wonGame){
+        SetTitle(TITLE_BASE + TITLE_ADD_WIN);
+        messageText->SetLabel(MESSAGE_WIN);
+    } else{
+        SetTitle(TITLE_BASE + TITLE_ADD_LOSE);
+        messageText->SetLabel(MESSAGE_LOSE);
+    }
+}
+
+
+int random_int(int minNumber, int maxNumber){
+    // obtaining a random number from hardware
+    std::random_device randomDevice;
+    // seed the Mersenne Twister engine (what ??)
+    std::mt19937 generator(randomDevice());
+    // defining a uniform integer distrubution for the desired range (what?? i am not understanding this)
+    std::uniform_int_distribution<> distribution(minNumber, maxNumber);
+    return distribution(generator);
 }
--- src/MainFrame.hpp
+++ src/MainFrame.hpp
@@ -4,8 +4,8 @@
 #include <string>
 
 
-// all the text
-constexpr std::string TITLE_BASE = "test";
+// all the constants
+constexpr std::string TITLE_BASE = "Higher Or Lower";
 constexpr std::string TITLE_LOWER = "Lower!!";
 constexpr std::string TITLE_HIGHER = "Higher!!";
 constexpr std::string TITLE_ADD_BEGIN = "!!";
@@ -18,17 +18,25 @@ constexpr std::string MESSAGE_HIGHER = "It's higher";
 constexpr std::string MESSAGE_LOSE = "You lost!";
 constexpr std::string MESSAGE_WIN = "You won!";
 
-constexpr std::string SUBMESSAGE_TRIES = " tries left";
+constexpr std::string SUBMESSAGE_TRIES = "{} tries left";
 constexpr std::string SUBMESSAGE_TRY = " try left";
-constexpr std::string SUBMESSAGE_REVEAL = "It was ";
+constexpr std::string SUBMESSAGE_REVEAL = "It was {}";
 
 constexpr std::string BUTTON_GUESS = "Guess";
 constexpr std::string BUTTON_REPLAY = "Replay";
+
+constexpr int RANDOM_MIN = 0;
+constexpr int RANDOM_MAX = 100;
+constexpr int NUMBER_OF_TRIES = 5;
 
 
 
 class MainFrame : public wxFrame{
 private:
+    bool isGaming;
+    int randomNumberToGuess;
+    int triesLeft;
+
     wxPanel* mainPanel;
     wxStaticText* messageText;
     wxStaticText* submessageText;
@@ -40,7 +48,8 @@ private:
     void setup_binds();
 
     void reset_game();
-    void finish_game();
+    void finish_game(bool wonGame);
+    void on_mainButton_pressed();
 
 public:
     MainFrame();
